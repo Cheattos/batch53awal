78de3f52d8c00118715adf8d00d3d9be3b1a939d
Cheattos <rrqlemin@gmail.com>
2024-03-27 14:56:12 +0700

    prisma menyenangkan

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'di4ysnubj',
+    api_key: '927126312432657',
+    api_secret: 'qXU1fflr5LxRkpOn39TkVQaSIrE'
+});
+
+export default cloudinary
